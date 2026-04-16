3195b05e8ccab1ddc55449e9a1cf1f971aae5ba4
Lisandro <disistolisandro037@gmail.com>
2026-04-16 12:47:17 +0200

    Cambio temporal para luego agregar Verificación (Back-End)

--- src/client/index.ts
+++ src/client/index.ts
@@ -4,17 +4,19 @@ const form = document.getElementById("login") as HTMLFormElement;
 
 async function sendData() {
   // Associate the FormData object with the form element
+  // Create (1) and convert (2) the FormData entries into a plain object
   const formData = new FormData(form);
   const loginData = Object.fromEntries(formData.entries());
 
+  if (verifyCredentialValidity(loginData) === true) {
+    const response = await fetch("/api/login", {
+      method: "POST",
+      body: JSON.stringify(loginData),
+    });
 
-  const response = await fetch("/api/login", {
-    method: "POST",
-    body: JSON.stringify(loginData),
-  });
-
-  const msg = response.ok ? "Login Aconseguit" : `Login no conseguit ${response.statusText}`;
-  document.getElementById("msgSpot")!.innerText = msg;
+    const msg = response.ok ? "Login Aconseguit" : `Login no conseguit ${response.statusText}`;
+    document.getElementById("msgSpot")!.innerText = msg;
+  }
 }
 
 // Take over form submission
@@ -22,3 +24,11 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   sendData();
 });
+
+
+function verifyCredentialValidity(credentials: any ) :boolean {
+  
+  //Meter verificación de login (¡LISANDRO!)
+
+  return true;
+} 
