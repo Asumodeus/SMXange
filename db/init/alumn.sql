a3f34d81e84db9fc6d62ff84556851f3fb4ce572
jofremontlleo-creator <jofre.montlleo@stnicolau.com>
2026-04-17 09:51:27 +0200

    Update alumn.sql

--- db/init/alumn.sql
+++ db/init/alumn.sql
@@ -1,16 +1,33 @@
-CREATE TABLE ALUMN (
-    id_alumne INT PRIMARY KEY,
-    first_name VARCHAR(50)
+CREATE TABLE Preguntes_de_seguretat (
+IDps INT NOT NULL,
+Com_et_dius VARCHAR(30),
+El_teu_numero_de_telefon VARCHAR(20),
+El_teu_mail VARCHAR(60),
+PRIMARY KEY (IDps)
 );
 
-INSERT INTO ALUMN (id_alumne, first_name) VALUES 
-    (1, 'Waverly'),
-    (2, 'Adelind'),
-    (3, 'Godfrey'),
-    (4, 'Chrysler'),
-    (5, 'Englebert'),
-    (6, 'Blancha'),
-    (7, 'Albert'),
-    (8, 'Zacherie'),
-    (9, 'Bryana'),
-    (10, 'Bogart');
+CREATE TABLE Usuari (
+IDusuari INT NOT NULL,
+Nom VARCHAR(50) NOT NULL,
+Cognom VARCHAR(50) NOT NULL,
+Numero_de_telefon VARCHAR(20),
+Username VARCHAR(50) NOT NULL,
+Password VARCHAR(50) NOT NULL,
+Mail VARCHAR(100) NOT NULL,
+IDps INT,
+PRIMARY KEY (IDusuari), 
+CONSTRAINT fk_preguntes_de_seguretat
+FOREIGN KEY (IDps) 
+REFERENCES Preguntes_de_seguretat(IDps)
+);
+
+CREATE TABLE Login (
+IDlogin INT NOT NULL,
+Username VARCHAR(50),
+Password VARCHAR(255),
+IDusuari INT, 
+PRIMARY KEY (IDlogin),
+CONSTRAINT fk_user
+FOREIGN KEY (IDusuari)
+REFERENCES Usuari(IDusuari) 
+);
