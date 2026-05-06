a7d979738b65357e05f1e77255195892f45adbeb
JDT26 <jordandancuart2@gmail.com>
2026-05-06 10:44:42 +0200

    Soluciön error academia

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,5 @@
 import landingPage from "../client/Portada.html"
-import loginPage from "../client/index.html";
+import loginPage from "../client/index.html"
 import forgotPswd from "../client/forgotPswd.html"
 import registerPage from "../client/register.html"
 import pagina_principal from "../client/pagina_principal.html"
@@ -29,10 +29,10 @@ Bun.serve({
 
         "/principal": pagina_principal,
 
-        "/academia1_1":  academiaPage,
-        "/academia1_2":  academiaPage2,
-        "/academia1_3":  academiaPage3,
-        "/academia1_4":  academiaPage4,
+        "/academia_1_1":  academiaPage,
+        "/academia_1_2":  academiaPage2,
+        "/academia_1_3":  academiaPage3,
+        "/academia_1_4":  academiaPage4,
 
         "/api/login": async (req) => {
             return await api.loginVerification(req);
